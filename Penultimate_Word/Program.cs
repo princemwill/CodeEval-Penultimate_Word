e0d8424174a8c0865f2abe4d8081a5b9246127ab
princemwill <princemwilliams@gmail.com>
2017-04-06 12:56:36 -0400

    added few lines to default file reading script. outputs penultimate word.

--- Penultimate_Word/Program.cs
+++ Penultimate_Word/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Penultimate_Word
     {
         static void Main(string[] args)
         {
+            
+        using (StreamReader reader = File.OpenText(@"C:\Users\princ\Documents\Notes\CodeEval_PenultimateWord.txt"))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+                    string[] words = line.Split(' ');
+
+                            Console.WriteLine(words[(words.Length) - 2]);                    
+        }
+        
+            Console.ReadKey();
+            
         }
     }
 }
